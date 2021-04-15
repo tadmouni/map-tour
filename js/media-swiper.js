8fe65c6f9f0ef24b0d667947bd12b3b8bb79c02a
tadmouni <58752802+tadmouni@users.noreply.github.com>
2021-04-15 15:17:08 -0500

    Added arrows for swiper

--- js/media-swiper.js
+++ js/media-swiper.js
@@ -3,9 +3,15 @@ var swiper = new Swiper('.swiper-container', {
     pagination: {
       el: '.swiper-pagination',
       clickable: true,
+      
       renderBullet: function (index, className) {
         return '<span class="' + className + '">' + '</span>';
       },
+    },
+
+    navigation: {
+      nextEl: '.swiper-button-next-unique',
+      prevEl: '.swiper-button-prev-unique'
     },
         mousewheel: true,
         keyboard: true,
